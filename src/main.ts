2d777eded305fb02e739d98070fe2d16a7795f4d
Tristan Morrison <tmorrison@esri.com>
2023-06-27 10:50:43 -0400

    Add main app code

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import WebMap from "@arcgis/core/WebMap"
+import MapView from "@arcgis/core/views/MapView"
+import Editor from "@arcgis/core/widgets/Editor"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const webmap = new WebMap({
+  portalItem: {
+    id: "f1f5dbb40369490bb2a6a4b11f8e78a6"
+  }
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const view = new MapView({
+  container: "viewDiv",
+  map: webmap
+});
+
+const editor = new Editor({ view })
+view.ui.add(editor, "top-right");
